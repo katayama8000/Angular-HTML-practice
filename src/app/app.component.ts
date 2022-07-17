8285a0d23ecdb4743c15114a6d2be706a7ae8819
katayama <tattu.0310@gmail.com>
2022-07-17 11:33:39 +0900

    ⚡️ バインディング

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-practice';
   name = 'Angular';
+  imageUrl =
+    'https://cdn.searchenginejournal.com/wp-content/uploads/2022/04/reverse-image-search-627b7e49986b0-sej-760x400.png';
+  date!: Date | string;
+
+  myName = '片山';
+
+  test() {
+    this.date = '';
+  }
+
+  show() {
+    this.date = new Date();
+  }
 }
