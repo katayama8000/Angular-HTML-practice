30ba79b55895450cf899dead124e73693d0226c0
katayama <tattu.0310@gmail.com>
2022-07-17 10:53:01 +0900

    🎉 first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //template: '<div><h1>hello</h1><div>{{name}}</div></div>',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-practice';
+  name = 'Angular';
 }
