31d14bb89894901804d273b3551aa85e541b3846
katayama <tattu.0310@gmail.com>
2022-07-17 15:33:20 +0900

    ✨ filter

--- src/app/testhttp/testhttp.component.ts
+++ src/app/testhttp/testhttp.component.ts
@@ -1,27 +1,40 @@
 import { TesthttpService } from './../testhttp.service';
 import { Component, OnInit } from '@angular/core';
-import {member} from "../type/member-type";
+import { member } from '../type/member-type';
+import { from } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-testhttp',
   templateUrl: './testhttp.component.html',
-  styleUrls: ['./testhttp.component.css']
+  styleUrls: ['./testhttp.component.css'],
 })
 export class TesthttpComponent implements OnInit {
+  constructor(private TesthttpService: TesthttpService) {}
 
-  constructor(private TesthttpService:TesthttpService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  member: member[] = [];
 
-  member:member[] = [];
+  source = from([
+    { name: 'sdfhd', age: 31 },
+    { name: 'Bob', age: 25 },
+    { name: 'dg', age: 31 },
+    { name: 'Bob', age: 25 },
+    { name: 'fhk', age: 31 },
+    { name: 'Bob', age: 25 },
+    { name: 'dgj', age: 31 },
+    { name: 'Bob', age: 25 },
+    { name: 'gjkl', age: 31 },
+    { name: 'Bob', age: 25 },
+  ]);
+
+  example = this.source.pipe(filter((person) => person.age >= 30));
+  subscribe = this.example.subscribe((val) => console.log(`Over 30: ${val.name}`));
 
   getjsonplaceholder() {
-    this.TesthttpService.getJson().subscribe(
-      (data:member) => {
-        this.member.push(data);
-      }
-    );
+    this.TesthttpService.getJson().subscribe((data: member) => {
+      this.member.push(data);
+    });
   }
-
 }
